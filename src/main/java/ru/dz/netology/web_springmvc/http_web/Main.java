aa6b4b6e5e189de3cb3e92a850e87757225ecb0f
Artur Gerc <artur.gerc@yandex.ru>
2025-03-23 14:45:38 +0500

    Initial commit

---
+++ src/main/java/ru/dz/netology/web_springmvc/http_web/Main.java
@@ -0,0 +1,10 @@
+package ru.dz.netology.web_springmvc.http_web;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server(9999);
+        server.start();
+
+    }
+}
